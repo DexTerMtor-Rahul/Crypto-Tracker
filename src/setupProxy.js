9a02da6c70a4b915261b2a3533646a8d6ce94a80
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-01-24 22:11:48 +0530

    Se

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,7 +7,8 @@ module.exports = function (app) {
       target: "https://api.coingecko.com",
       changeOrigin: true,
       onProxyRes(proxyRes) {
-        proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+        proxyRes.headers["Access-Control-Allow-Origin"] =
+          "https://dextermtor-rahul.github.io/";
       },
     })
   );
